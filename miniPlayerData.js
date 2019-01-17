4ac3bf1e2c24c3e7cb08af1a143aa5192ce4b59f
AntoCr <add8596.@gmail.com>
2019-01-17 22:20:20 +0100

    updating version

--- miniPlayerData.js
+++ miniPlayerData.js
@@ -3,13 +3,13 @@ klynt.miniPlayerData = {
     "resumePlaybackWording": "Resume playback?",
     "fullscreenInfoWording": "This program will launch in fullscreen",
     "yesWording": "Yes",
-    "title": "Visioni Future",
+    "title": "Visioni Future - Twitter",
     "thumbnail": "Player/css/editor/img/mini-player.jpg",
     "noWording": "No",
     "analyticsKey": "",
-    "downloadAppWording": "Download App",
     "redirectToMobileApp": "auto",
-    "launchAppWording": "Then Launch Project",
+    "downloadAppWording": "Download App",
     "url": "",
+    "launchAppWording": "Then Launch Project",
     "description": "YouTube"
 }
